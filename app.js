7b4e0d4f694728a63752940fe1c3217d451e6d26
Janaina da Silva Rodrigues <145395931+dasilvajanaine@users.noreply.github.com>
2024-10-31 14:56:54 -0300

    31/10

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-function criaCartao(categoria, pergunta,resposta){
+function criaCartao(categoria,pergunta,resposta){ 
+     let container = document.getElementById('container');
+      let cartao = document.createElement('article'); 
+      cartao.classname = 'cartao';
 
-    
-}
+       cartao.innerHTML = 
+                       <div class="cartao__conteudo"> 
+                         <h3>${categoria}</h3> 
+                         <div class="cartao__conteudo__pergunta">
+                        <p>${pergunta}</p>
+                        </div>
+                        <div class="cartao__conteudo__resposta">
+         <p>${respostas}</p> 
+          </div>
+           `
+             container.appendChild(cartao);
+     {
+
+     }    
